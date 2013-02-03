b98a51720703c7309ae627751ff0935b47ba4672
syouga <syouga.curry@gmail.com>
2013-02-03 20:53:52 +0900

    rails create application

---
+++ rentals/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rentals::Application.config.secret_token = '2a1aaab4b4298b50fa8b4d546ee850776036c5618d51fe6c3deb1756cd097999c2c1f872c7b88ff83e752f054ae7b1336be9466ad32b0be425e469d54277c460'
